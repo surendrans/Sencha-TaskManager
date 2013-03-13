eef7e85154a4c051cc3247a1b681066233c4408a
Surendran <2007.jack@gmail.com>
2013-03-13 11:18:04 +0530

    Initial commit

---
+++ app/view/Main.js
@@ -0,0 +1,31 @@
+Ext.define('Sencha-TaskManager.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'HOME',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Tasks'
+                },
+
+                html: [
+                    "TODO : Create a task manager app using Sencha touch"
+                ].join("")
+            }
+        ]
+    }
+});
